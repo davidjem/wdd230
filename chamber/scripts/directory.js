6db270f0928e9b3952749b7153330950cc9d3edc
davidjem <davidjulian.es@gmail.com>
2023-06-16 16:14:22 -0600

    json updated

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,3 +1,20 @@
-// const data = fetch("/data/test.json")
-//   .then(response => response.json())
-//   .then(json => console.table(json));
+const url = 'https://davidjem.github.io/wdd230/chamber/data/data.json';
+
+// const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.businesses);  // note that we reference the prophet array of the data object given the structure of the json file
+    
+    
+    // displayProphets(data.prophets);
+
+
+  }
+  
+  
+  getProphetData();
+
+
+
