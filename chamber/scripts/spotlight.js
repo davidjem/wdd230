c33c84ab5d0987aef5ab602d17c665a122c52c09
davidjem <davidjulian.es@gmail.com>
2023-06-30 18:06:07 -0600

    contact us added

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -52,7 +52,6 @@ const displayBusiness = (businesses) => {
   });
 };
 
-// Helper function to shuffle an array
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
