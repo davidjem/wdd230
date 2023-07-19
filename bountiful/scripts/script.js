4ea2e254688389aaff88cc6b4925d32ba7160842
davidjem <davidjulian.es@gmail.com>
2023-07-19 17:24:03 -0600

    info added

--- bountiful/scripts/script.js
+++ bountiful/scripts/script.js
@@ -108,5 +108,44 @@ let display = `${month}-${day}-${year}`;
 
 document.getElementById("timeStamp").innerHTML = display;
 
+//////////////////////////////////////////////////////////////////////////////////
 
 
+
+ // This function increases the count
+ function clickButtonFunction() {
+
+  console.log('run');
+
+  //Check to see if the localstorage variable exists. If it does not, this is the first time coming to the page, and we need to initalize it to 0
+  if (localStorage.getItem("clickCounter") === null) {
+    // Store
+    localStorage.setItem("clickCounter", "0");
+  }
+
+  // Get the value from local storage
+  var value = parseInt(localStorage.getItem("clickCounter"));
+
+  // Incrememtnt the count by 1
+  var newValue = value + 1
+
+  //Write the value back to local storage
+  localStorage.setItem("clickCounter", newValue);
+
+
+  // Write the value to the div
+  document.getElementById("clickCounter").innerHTML = newValue
+
+  document.getElementById("submittedDrinks").innerHTML = newValue
+
+}
+
+// Reset local storage to 0
+function clickReset() {
+
+  localStorage.setItem("clickCounter", "0");
+  // Write the value to the div
+  document.getElementById("clickCounter").innerHTML = '0'
+
+
+}
