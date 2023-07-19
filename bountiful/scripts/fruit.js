1b0ccc0338942de791173b0b02d1ecbe114a70c1
davidjem <davidjulian.es@gmail.com>
2023-07-18 21:42:20 -0600

    update

--- bountiful/scripts/fruit.js
+++ bountiful/scripts/fruit.js
@@ -1,6 +1,9 @@
-// const url2 = "https://davidjem.github.io/wdd230/chamber/data/data.json";
 
 const url2 = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+const fruitdata = getBusinessData();
+
+
+
 
 async function getBusinessData() {
   const response = await fetch(url2);
@@ -15,56 +18,87 @@ async function getBusinessData() {
   //   item1.name, item2.name, item3.name
 
   // ]
- 
-  console.log(data);  
+
+  // console.log(data);
 
   var selectElement = document.getElementById('mySelect');
 
-  data.map(item => mySelect.appendChild(new Option(item.name)).cloneNode(true));
+  data.map(item => mySelect1.appendChild(new Option(item.name, item.name)).cloneNode(true));
 
+  data.map(item => mySelect2.appendChild(new Option(item.name, item.name)).cloneNode(true));
 
-
-
-
-
-
-
-
-  // for ( var a  in mix){
-  //   console.log(a);
-  //   let checkbox = document.createElement('input')
-  //   let label = document.createElement('label')
-  //   let line = document.createElement('br')
-  //   label.appendChild(document.createTextNode(mix[a]))
-  //   checkbox.type = 'radio';
-  //   checkbox.name = "myradio";
-  //   wrapElement.appendChild(checkbox);
-  //   wrapElement.appendChild(label)
-  //   wrapElement.appendChild(line);  
-    
-  // }
-
+  data.map(item => mySelect3.appendChild(new Option(item.name, item.name)).cloneNode(true));
 
   
 
 
-
-
-
-
-
-
-
-
-
-    
 }
-
-
 
 
 getBusinessData();
 
 
-// src="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag/dist/js/multi-select-tag.js";
-// new MultiSelectTag('mySelect');  
+
+
+function getOption(id, text) {
+  selectElement = document.querySelector(id);
+  output = selectElement.options[selectElement.selectedIndex].value;
+  document.querySelector(text).textContent = output;
+  console.log(output);
+  
+}
+
+function search(nameKey, myArray){
+  for (let i=0; i < myArray.length; i++) {
+      if (myArray[i].name === nameKey) {
+          return myArray[i];
+      }
+  }
+}
+
+function add(a, b, c){
+  const total = a + b + c;
+  return total;
+ }
+
+function surveyResult (){
+  const firstSelection = document.querySelector('.output1').innerText;
+  const secondSelection = document.querySelector('.output2').innerText;
+  const thirdSelection = document.querySelector('.output3').innerText;
+  // const timestamp = Date.now();
+
+  console.log(firstSelection)
+  console.log(secondSelection)
+  console.log(thirdSelection)
+  async function findArray() {
+    const response = await fetch(url2);
+    const data = await response.json();
+    // console.log(data); 
+    const resultObject1 = search(firstSelection, data);
+    const resultObject2 = search(secondSelection, data);
+    const resultObject3 = search(thirdSelection, data);
+
+  const totalCalories = add(resultObject1.nutritions.calories,resultObject2.nutritions.calories,resultObject3.nutritions.calories)
+  const totalCarbs = add(resultObject1.nutritions.carbohydrates,resultObject2.nutritions.carbohydrates,resultObject3.nutritions.carbohydrates)
+  const totalFat = add (resultObject1.nutritions.fat,resultObject2.nutritions.fat,resultObject3.nutritions.fat)
+  const totalProtein = add(resultObject1.nutritions.protein,resultObject2.nutritions.protein,resultObject3.nutritions.protein)
+  const totalSugar = add(resultObject1.nutritions.sugar,resultObject2.nutritions.sugar,resultObject3.nutritions.sugar)
+  
+
+    console.log(resultObject1);
+    console.log(resultObject2);
+    console.log(resultObject3);
+
+
+    console.log(totalCalories);
+    console.log(totalCarbs);
+    console.log(Math.round(totalFat * 100) / 100);
+    console.log(Math.round(totalProtein * 100 ) );
+    console.log(Math.round(totalSugar * 100 ) / 100 );
+    // console.log(timestamp);
+  }
+  
+  findArray();
+}
+
+
